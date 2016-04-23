61f6780201f85f5e545ca1e2dd3507adbc91e82f
´saramgs28´ <smontero@stevens.edu>
2016-04-22 21:20:08 -0400

    I couldnt get to work audio record

--- app/src/main/java/com/andresrcb/gcmtest/ConversationActivity.java
+++ app/src/main/java/com/andresrcb/gcmtest/ConversationActivity.java
@@ -3,16 +3,23 @@ package com.andresrcb.gcmtest;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class ConversationActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+import java.io.IOException;
+
+public class ConversationActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ChatAdapter chatAdapter;
     private ListView listView;
@@ -28,6 +35,15 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
     private static Bitmap bmp;
     private static ImageView img;
 
+    //AUDIO
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4,             MediaRecorder.OutputFormat.THREE_GPP };
+    private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +53,9 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
         buttonSendVideo = (Button) findViewById(R.id.button_video);
         buttonSendPicture = (Button) findViewById(R.id.button_picture);
 
-        buttonSendAudio.setOnClickListener(this);
-        buttonSendVideo.setOnClickListener(this);
-        buttonSendPicture.setOnClickListener(this);
+        //buttonSendAudio.setOnClickListener(this);
+        //buttonSendVideo.setOnClickListener(this);
+        //buttonSendPicture.setOnClickListener(this);
 
         listView = (ListView) findViewById(R.id.list_messages);
         chatAdapter = new ChatAdapter(this, R.layout.fragment_chat_singlemessage_left);
@@ -47,6 +63,22 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
 
         listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
         listView.setAdapter(chatAdapter);
+
+        //AUDIO PRUEBA 2
+        buttonSendAudio.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        startRecording();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        stopRecording();
+                        break;
+                }
+                return false;
+            }
+        });
 
         //to scroll the list view to bottom on data change. NO HAGO SCROLL
         /*chatAdapter.registerDataSetObserver(new DataSetObserver() {
@@ -57,21 +89,58 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
             }
         });*/
     }
+    private String getFilename(){
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+    private void startRecording(){
+        if( recorder == null ) {
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(output_formats[currentFormat]);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setOutputFile(getFilename());
+        }
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void stopRecording(){
+        if(null != recorder){
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+            Toast.makeText(this, "SE HA GRABADO", Toast.LENGTH_LONG);
+            chatAdapter.add(new ChatMessage(side, "AUDIO", fileType));
+            side = !side;
+            recorder = null;
+        }
+    }
     /*private boolean sendChatMessage(){
         return true;
     }*/
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button_audio:
+            /*case R.id.button_audio:
                 fileType="audio";
-                sendAudio();
+                RecordAudio();
                 chatAdapter.add(new ChatMessage(side, "AUDIO", fileType));
                 side = !side;
-            break;
+            break;*/
             case R.id.button_picture:
                 fileType="picture";
-                sendPicture();
+                takePicture();
                 chatAdapter.add(new ChatMessage(side, "PICTURE",fileType));
                 side = !side;
                 break;
@@ -81,16 +150,18 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
                 side = !side;
                 break;
         }
-        //sendChatMessage();
+        //sendChatMessage(); //SEND THE PICTURE OR VIDEO
     }
-    public void sendPicture()
+
+    //IMAGE
+    public void takePicture()
     {
         //DEBO RETURN PICTURE
         i= new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //Open camera
         //i= new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         startActivityForResult(i, cons);//para recibir datos de esta actividad
     }
-    //UNA SOLA IMAGEN
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data); //No alterar lo que esta escrito. Si no lo pongo, no hace el contenido
@@ -102,10 +173,6 @@ public class ConversationActivity extends AppCompatActivity implements View.OnCl
         }
     }
 
-    public void sendAudio()
-    {
-
-    }
    /* public void sendTextMessage(){
         RequestQueue queue = Volley.newRequestQueue(this);
         String toPhone = "";
